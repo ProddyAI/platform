7133a692a64e96f3483514c0ce0ebbb602771c2c
SanjilDsilva <sanjildsilva@gmail.com>
2026-01-07 09:18:25 +0530

    Resolved merge conflicts in command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -26,17 +26,6 @@ Command.displayName = CommandPrimitive.displayName;
 interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
-<<<<<<< HEAD
-  return (
-    <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command defaultValue="" shouldFilter={false} className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children}
-        </Command>
-      </DialogContent>
-    </Dialog>
-  );
-=======
 	return (
 		<Dialog {...props}>
 			<DialogContent className="overflow-hidden p-0 shadow-lg">
@@ -46,7 +35,6 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
 			</DialogContent>
 		</Dialog>
 	);
->>>>>>> origin/main
 };
 
 const CommandInput = React.forwardRef<
