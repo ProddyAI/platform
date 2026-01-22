6f2c84e67f9441c3d1d34220ffb7404924c6cf93
George Bobby <georgebobbyawesome@gmail.com>
2026-01-22 12:44:11 +0530

    lint for files

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -11,12 +11,7 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
 		<Dialog>
 			<DialogTrigger>
 				<div className="relative my-2 max-w-[360px] cursor-zoom-in overflow-hidden rounded-lg border">
-					{/* eslint-disable-next-line @next/next/no-img-element */}
-					<img
-						src={url}
-						alt="Message image"
-						className="size-full rounded-md object-cover"
-					/>
+					<img src={url} className="size-full rounded-md object-cover" />
 				</div>
 			</DialogTrigger>
 
@@ -24,12 +19,7 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
 				isThumbnail
 				className="max-w-[800px] border-none bg-transparent p-0 shadow-none"
 			>
-				{/* eslint-disable-next-line @next/next/no-img-element */}
-				<img
-					src={url}
-					alt="Message image"
-					className="size-full rounded-md object-cover"
-				/>
+				<img src={url} className="size-full rounded-md object-cover" />
 			</DialogContent>
 		</Dialog>
 	);
