e8ebe84c97b1e6d1099c57d247c8355d4bf906a9
George Bobby <mailto.georgebobby@gmail.com>
2026-02-14 18:03:28 +0530

    tests

--- tests/test-message.ts
+++ tests/test-message.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env tsx
 
 import { ConvexHttpClient } from "convex/browser";
-import { api } from "./convex/_generated/api";
+import { api } from "../convex/_generated/api";
 
 // Use the cloud deployment
 const deploymentUrl = process.env.NEXT_PUBLIC_CONVEX_URL || "https://friendly-marmot-282.convex.cloud";
