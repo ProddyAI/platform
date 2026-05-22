5514900455aa739a337cc1bac319305cf2eb5b5e
amaltomajith <work.amaltom@gmail.com>
2026-05-22 06:05:27 +0530

    Avoid bare importScripts global

--- public/OneSignalSDKUpdaterWorker.js
+++ public/OneSignalSDKUpdaterWorker.js
@@ -1,3 +1,1 @@
-/* global importScripts */
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+globalThis.importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
